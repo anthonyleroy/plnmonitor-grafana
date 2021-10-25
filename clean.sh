822818ce9a02e404294af09532a509cf9300f8c0
Anthony Leroy <anleroy@ulb.ac.be>
2021-10-25 22:15:50 +0200

    clean script also deletes previous box dashboards

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker-compose down
-echo "Removing database persistent storage"
-rm -rf ./pln_dbdata/*
+
 echo "Removing dashboards"
-rm -rf ./provisioning/dashboards/plnmonitor/*.json
+rm -f ./provisioning/dashboards/*.json
+rm -f ./provisioning/dashboards/boxes/*.json 
