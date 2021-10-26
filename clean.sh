5240c57416cda0ee2277b9fd7d498742f41adf26
Anthony Leroy <anleroy@ulb.ac.be>
2021-10-26 19:28:14 +0200

    Generating Grafana dashboards for each TDB Publisher

--- clean.sh
+++ clean.sh
@@ -5,3 +5,4 @@ docker-compose down
 echo "Removing dashboards"
 rm -f ./provisioning/dashboards/*.json
 rm -f ./provisioning/dashboards/boxes/*.json 
+rm -f ./provisioning/dashboards/tdb_publishers/*.json 
