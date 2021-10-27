6af22f6f83f5822b578c648feecea074e98cc3c5
Anthony Leroy <anleroy@ulb.ac.be>
2021-10-27 12:49:44 +0200

    Set default home dashboard

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ docker-compose run plnmondaemon /usr/bin/java -jar /opt/plnmonitor-daemon.jar
 
 docker-compose up -d plnmonwebapp
 
+cp -f ./template/home.json ./provisioning/dashboards/home.json
 echo "plnmonitor is now available at : http://localhost:3000/"
